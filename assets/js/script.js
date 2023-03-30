619248484abcf7deac96c34267c8fa5d2cceab82
raffaele-catalano <raf.catalano@gmail.com>
2023-03-30 18:00:28 +0200

    added click on squares function and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,10 @@ function reset() {
 
 
 // FUNCTIONS
+/**
+ * this function generates the playground of the game
+ * @param {squares amount based on level of difficulty} squareNumbers 
+ */
 function playGroundGenerator(squareNumbers){
     const grid = document.createElement('div');
     grid.className = 'grid';
@@ -43,12 +47,30 @@ function playGroundGenerator(squareNumbers){
 
     main.append(grid);
 }
-
+/////////////////////////////////////////////////////////////////////////
+/**
+ * this function generate a new square
+ * @param {square amount based on level of difficulty} squareNumbers 
+ * @param {square value} id 
+ * @returns generate a new square
+ */
 function squareGenerator(squareNumbers, id) {
     const square = document.createElement('div');
     square.className = 'square';
     square.classList.add('square' + squareNumbers)
     square.squareId = id;
 
+    square.innerHTML = `<span>${id}</span>`
+
+    square.addEventListener('click', clickedSquare)
+
     return square;
+}
+/////////////////////////////////////////////////////////////////////////
+/**
+ * this function make squares change color by clicking on them
+ */
+function clickedSquare() {
+        // console.log('valore dello square -->', this.squareId);
+    this.classList.toggle('clicked');
 }
