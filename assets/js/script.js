b6aa959a3feb73fb53a0eafced1cf9126b1b83c9
raffaele-catalano <raf.catalano@gmail.com>
2023-03-30 17:39:20 +0200

    added grid & squares generator functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // ELEMENTS
-const main        = document.querySelector('main-wrapper');
+const main        = document.querySelector('.grid-wrapper');
 const playBtn     = document.getElementById('play');
 const resetBtn    = document.getElementById('reset');
 const levelSelect = document.querySelector('select');
@@ -13,13 +13,42 @@ const gameLevels  = [100, 81, 49];
 // PLAY GAME
 playBtn.addEventListener('click', play)
 
-function play (){
-        console.log('play');
+function play() {
+        // console.log('play');
+
+    const squareNumbers = gameLevels[levelSelect.value];
+        // console.log('square numbers -->', squareNumbers);
+    playGroundGenerator(squareNumbers)
+
 }
 
 // RESET GAME
 resetBtn.addEventListener('click', reset)
 
-function reset () {
-        console.log('reset');
+function reset() {
+        // console.log('reset');
+    main.innerHTML = '';
+}
+
+
+// FUNCTIONS
+function playGroundGenerator(squareNumbers){
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+        // console.log(grid);
+    for (let i = 1; i <= squareNumbers; i++) {
+        const square = squareGenerator(squareNumbers, i);
+        grid.append(square);
+    }
+
+    main.append(grid);
+}
+
+function squareGenerator(squareNumbers, id) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    square.classList.add('square' + squareNumbers)
+    square.squareId = id;
+
+    return square;
 }
